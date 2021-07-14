697ab46cf77289d4c01cfe6aee1b27ccedbb1051
adithyasunil26 <adithyasunil26@gmail.com>
2021-07-15 00:25:20 +0400

    Copying generated sram to fusesoc generated

--- bsg_fakeram_gen.py
+++ bsg_fakeram_gen.py
@@ -8,24 +8,25 @@ class BSGRoundRobinArbGenerator(Generator):
         
         cwd = self.files_root
         
-        rc = subprocess.call(['pwd'], cwd=cwd)
-        
         args = ['cp', '-rf','../bsg_fakeram_gen_0-r1/Makefile','Makefile']
-        
         rc = subprocess.call(args, cwd=cwd)
 
         args = ['make', 'tools']
-        
         rc = subprocess.call(args, cwd=cwd)
         
         args = ['make', 'run', 'CONFIG=', path_to_cfg]
-        
         rc = subprocess.call(args, cwd=cwd)
         
+        args = ['cp', '-rf','results/sram_8x512_1rw/sram_8x512_1rw.v','../generated/bsg_fakeram-gen_0/sram_8x512_1rw.v']
+        rc = subprocess.call(args, cwd=cwd)
+        
+        args = ['cp', '-rf','results/sram_32x32_1rw/sram_32x32_1rw.v','../generated/bsg_fakeram-gen_0/sram_32x32_1rw.v']
+        rc = subprocess.call(args, cwd=cwd)
+
         if rc:
             exit(1)
         
-        self.add_files([{ 'results/sram_8x512_1rw/sram_8x512_1rw.v' : {'file_type' : 'verilogSource'}}, { 'results/sram_32x32_1rw/sram_32x32_1rw.v' : {'file_type' : 'verilogSource'}}])
+        self.add_files([{ 'sram_8x512_1rw.v' : {'file_type' : 'verilogSource'}}, { 'sram_32x32_1rw.v' : {'file_type' : 'verilogSource'}}])
 
 g = BSGRoundRobinArbGenerator()
 g.run()
